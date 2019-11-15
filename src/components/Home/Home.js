70ab740fb0b9a9f58c5c39f40d517912e1d6b8ab
임근석 <rmstjr1030@nate.com>
2019-11-15 12:19:40 +0000

    UpdateCode

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,87 @@
 import React from 'react';
 import { Grid, Header, Image, Card, Label, Container, Accordion } from 'semantic-ui-react';
-import { Scriptable } from 'react-chartjs-2';
+import { Radar } from 'react-chartjs-2';
 import './Home.css';
+
+const data = {
+	labels: ['Syntax', 'Debuging', 'Architecture', 'Expansion', 'CodeReview'],
+	datasets: [
+		{
+			label: 'C',
+			backgroundColor: 'rgba(20,148,255,0.2)',
+			borderColor: 'rgba(20,148,255,1)',
+			pointBackgroundColor: 'rgba(20,148,255,1)',
+			pointBorderColor: '#fff',
+			pointHoverBackgroundColor: '#fff',
+			pointHoverBorderColor: 'rgba(20,148,255,1)',
+			data: [82, 81, 51, 58, 63]
+		},
+		{
+			label: 'Java',
+			backgroundColor: 'rgba(255,169,20,0.2)',
+			borderColor: 'rgba(255,169,20,1)',
+			pointBackgroundColor: 'rgba(1255,169,20,1)',
+			pointBorderColor: '#fff',
+			pointHoverBackgroundColor: '#fff',
+			pointHoverBorderColor: 'rgba(255,169,20,1)',
+			data: [88, 90, 91, 95, 92]
+		},
+		{
+			label: 'Python',
+			backgroundColor: 'rgba(239,220,5,0.2)',
+			borderColor: 'rgba(239,220,5,1)',
+			pointBackgroundColor: 'rgba(239,220,5,1)',
+			pointBorderColor: '#fff',
+			pointHoverBackgroundColor: '#fff',
+			pointHoverBorderColor: 'rgba(239,220,5,1)',
+			data: [85, 77, 67, 84, 81]
+		},
+		{
+			label: 'JavaScript',
+			backgroundColor: 'rgba(255,99,132,0.2)',
+			borderColor: 'rgba(255,99,132,1)',
+			pointBackgroundColor: 'rgba(255,99,132,1)',
+			pointBorderColor: '#fff',
+			pointHoverBackgroundColor: '#fff',
+			pointHoverBorderColor: 'rgba(255,99,132,1)',
+			data: [86, 72, 72, 97, 88]
+		}
+	]
+};
 
 class Home extends React.Component {
 	render() {
 		const panel = [
-			{ key: 0, title: 'C', content: {
+			{
+				key: 0,
+				title: 'C / C++ / C#',
+				content: {
 					content: (
 						<Grid>
 							<Grid.Row columns={4}>
+								<Grid.Column>
+									<Card href="https://github.com/Mineru98/Combination_of_Parentheses">
+										<Image
+											src="https://react.semantic-ui.com/images/wireframe/image.png"
+											wrapped
+											ui={false}
+										/>
+										<Card.Content>
+											<Card.Header>Combination of Parentheses</Card.Header>
+											<Card.Meta>
+												<span className="date">Deployed Apr 7, 2019</span>
+											</Card.Meta>
+											<Card.Description>
+												Matthew is a musician living in Nashville.
+											</Card.Description>
+										</Card.Content>
+										<Card.Content extra>
+											<Label as="a" color="white" image>
+												<Image src="img/icon_cplusplus.png" /> C
+											</Label>
+										</Card.Content>
+									</Card>
+								</Grid.Column>
 								<Grid.Column>
 									<Card href="https://github.com/Mineru98/CShap_Clock_Angle">
 										<Image
@@ -28,7 +100,7 @@ class Home extends React.Component {
 										</Card.Content>
 										<Card.Content extra>
 											<Label as="a" color="white" image>
-												<Image src="img/icon_python.png" /> C#
+												<Image src="img/icon_csharp.png" /> C#
 											</Label>
 										</Card.Content>
 									</Card>
@@ -38,8 +110,68 @@ class Home extends React.Component {
 					)
 				}
 			},
-			{ key: 1, title: 'Java', content: { content: <Label color="blue" content="Test" /> } },
-			{ key: 2, title: 'JavaScript', content: {
+			{
+				key: 1,
+				title: 'Java / Kotlin',
+				content: {
+					content: (
+						<Grid>
+							<Grid.Row columns={4}>
+								<Grid.Column>
+									<Card href="https://github.com/Mineru98/HopsAndroid">
+										<Image
+											src="https://react.semantic-ui.com/images/wireframe/image.png"
+											wrapped
+											ui={false}
+										/>
+										<Card.Content>
+											<Card.Header>Android App Project</Card.Header>
+											<Card.Meta>
+												<span className="date">Deployed Apr 7, 2019</span>
+											</Card.Meta>
+											<Card.Description>
+												Matthew is a musician living in Nashville.
+											</Card.Description>
+										</Card.Content>
+										<Card.Content extra>
+											<Label as="a" color="white" image>
+												<Image src="img/icon_java.png" /> Java
+											</Label>
+										</Card.Content>
+									</Card>
+								</Grid.Column>
+								<Grid.Column>
+									<Card href="https://github.com/Mineru98/DictionaryRandom/tree/master/kotlin">
+										<Image
+											src="https://react.semantic-ui.com/images/wireframe/image.png"
+											wrapped
+											ui={false}
+										/>
+										<Card.Content>
+											<Card.Header>Dictionary Random with Kotlin</Card.Header>
+											<Card.Meta>
+												<span className="date">Deployed Apr 7, 2019</span>
+											</Card.Meta>
+											<Card.Description>
+												Matthew is a musician living in Nashville.
+											</Card.Description>
+										</Card.Content>
+										<Card.Content extra>
+											<Label as="a" color="white" image>
+												<Image src="img/icon_kotlin.png" /> Kotlin
+											</Label>
+										</Card.Content>
+									</Card>
+								</Grid.Column>
+							</Grid.Row>
+						</Grid>
+					)
+				}
+			},
+			{
+				key: 2,
+				title: 'JavaScript / NodeJS / ReactJS',
+				content: {
 					content: (
 						<Grid>
 							<Grid.Row columns={4}>
@@ -61,7 +193,30 @@ class Home extends React.Component {
 										</Card.Content>
 										<Card.Content extra>
 											<Label as="a" color="white" image>
-												<Image src="img/icon_python.png" /> JavaScript
+												<Image src="img/icon_js.png" /> JavaScript
+											</Label>
+										</Card.Content>
+									</Card>
+								</Grid.Column>
+								<Grid.Column>
+									<Card href="https://github.com/Mineru98/DictionaryRandom/tree/master/nodejs">
+										<Image
+											src="https://react.semantic-ui.com/images/wireframe/image.png"
+											wrapped
+											ui={false}
+										/>
+										<Card.Content>
+											<Card.Header>Dictionary Random with NoteJS</Card.Header>
+											<Card.Meta>
+												<span className="date">Deployed Apr 7, 2019</span>
+											</Card.Meta>
+											<Card.Description>
+												Matthew is a musician living in Nashville.
+											</Card.Description>
+										</Card.Content>
+										<Card.Content extra>
+											<Label as="a" color="white" image>
+												<Image src="img/icon_nodejs.png" /> NodeJS
 											</Label>
 										</Card.Content>
 									</Card>
@@ -69,7 +224,8 @@ class Home extends React.Component {
 							</Grid.Row>
 						</Grid>
 					)
-				} },
+				}
+			},
 			{
 				key: 3,
 				title: 'Python',
@@ -146,8 +302,7 @@ class Home extends React.Component {
 						</Grid>
 					)
 				}
-			},
-			{ key: 4, title: 'Query', content: { content: <Label color="blue" content="Test" /> } }
+			}
 		];
 		return (
 			<div>
@@ -266,6 +421,11 @@ class Home extends React.Component {
 					<Container textAlign="left">
 						<Header id="default" as="h2" image="/img/language.svg" content="My Code Skil" />
 						<Grid>
+							<Grid.Row>
+								<Grid.Column>
+									<Radar data={data} height={100} />
+								</Grid.Column>
+							</Grid.Row>
 							<Grid.Row columns={1}>
 								<Grid.Column>
 									<Accordion defaultActiveIndex={[]} panels={panel} exclusive={false} />
